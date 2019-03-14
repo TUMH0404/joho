e6f803b666637410e19cb56f7553220715941aa5
TUMH0404 <t.ohnishi@tumh.ac.jp>
2019-03-14 20:48:20 +0900

    Create PT.py
    Assessment of motions for persons by visual testing

---
+++ PT.py
@@ -0,0 +1,39 @@
+########
+
+### 動作観察  #####
+
+def whiledef(text,n=0):
+    '''
+    動作観察を入力するためのプログラム
+    終わるときは"y"のみを入力してEnterをおす。
+    それ以外は，文字を入力する。
+    '''
+    rrr=""
+    while(1):
+        rr = input(text+str(n))
+        if rr=="y":
+            break
+        else:
+            n=n+1
+            rrr=rrr+rr+'\n'
+
+    return rrr
+
+
+
+def Posture_Move():
+    ## 定義 ##
+
+    bodymove = {"仰臥位":None,"座位":None,"立位":None,"起き上がり":None,"立ち上がり":None}
+
+
+    for qq in bodymove.keys():
+        nv = 0
+        coment = whiledef(f"{qq}について....{nv}\n")
+ 
+        nv+=1;       
+            
+        bodymove[qq] = coment
+        
+    
+    return bodymove
